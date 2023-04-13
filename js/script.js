61210841ef376a82b9910c316c2a6f2dcd7c7e35
Lilyyyc <124380100+Lilyyyc@users.noreply.github.com>
2023-04-13 18:29:59 +0000

    Finished

--- js/script.js
+++ js/script.js
@@ -10,13 +10,13 @@ function guessSent() {
  let correctNumber = Math.floor(Math.random() * MAX + MIN); 
  let guessedNumber = document.getElementById("user-guess").value;
 
- //if/then statement: if correctNumber is equal to guessedNumber
+ // if/then statement: if correctNumber is equal to guessedNumber
  if (correctNumber == guessedNumber) {
    document.getElementById("results").innerHTML = "That is correct! Good job!";
  }
 
-  //if/then statement: if correctNumber is not equal to guessedNumber
+  // if/then statement: if correctNumber is not equal to guessedNumber
 if (correctNumber != guessedNumber) {
-   document.getElementById("results").innerHTML = "Your guess not correct! My number was " + correctNumber + ".";
+   document.getElementById("results").innerHTML = "Your guess was not correct. My number was " + correctNumber + "," + "you got this, try again.";
  }
 }
